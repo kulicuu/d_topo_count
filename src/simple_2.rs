1e33a48c203c5491490ba6d5c8e15f7cdaf1829d
J Wylie Cullick <wylie@terebinth.is>
2022-08-04 10:43:30 -0500

    more test cases

--- src/simple_2.rs
+++ src/simple_2.rs
@@ -3,6 +3,22 @@ use std::collections::HashSet;
 const RESOLUTION: usize = 20;
 const SQUARE: usize = RESOLUTION * RESOLUTION;
 
+pub fn routine_300
+()
+{
+    let arr = build_space();
+    let count = count_neighborhoods(&arr);
+    println!("\n Count for space-1: {} \n", count);
+
+    let arr = build_space_2();
+    let count = count_neighborhoods(&arr);
+    println!("\n Count for space-2: {} \n", count);
+
+    let arr = build_space_3();
+    let count = count_neighborhoods(&arr);
+    println!("\n Count for space-3: {} \n", count);
+}
+
 fn get_create_nghd(
     nc: &mut Vec<HashSet<usize>>,
     el: usize,
@@ -32,14 +48,6 @@ fn get_nghd(
         }
     }
     return -1
-}
-
-pub fn routine_300
-()
-{
-    let arr = build_space();
-    let count = count_neighborhoods(&arr);
-    println!("\n Final count: {} \n", count);
 }
 
 fn count_neighborhoods(arr: &[bool; SQUARE])
@@ -109,13 +117,66 @@ fn build_space
     arr[((10 * RESOLUTION) + 11) as usize] = true; // 211
     arr[((10 * RESOLUTION) + 12) as usize] = true; // 212
     arr[((11 * RESOLUTION) + 11) as usize] = true; // 221
+    arr[((12 * RESOLUTION) + 11) as usize] = true; // 241
 
-    println!("arr {:?}", arr);
-    for (idx, el) in arr.into_iter().enumerate() {
-        if el == true {
-            println!("i, e {} {}", idx, el);
-        }
-        
-    }
+    arr
+}
+
+fn build_space_2
+()
+-> [bool; SQUARE as usize]
+{
+    let mut arr: [bool; SQUARE as usize] = [false; SQUARE as usize];
+
+    arr[((2 * RESOLUTION) + 3) as usize] = true; // 43
+    arr[((2 * RESOLUTION) + 4) as usize] = true; // 44
+    arr[((2 * RESOLUTION) + 5) as usize] = true; // 45
+    arr[((3 * RESOLUTION) + 3) as usize] = true; // 63
+    arr[((3 * RESOLUTION) + 5) as usize] = true; // 65
+
+    // Now we make another small island 
+    arr[((10 * RESOLUTION) + 11) as usize] = true; // 211
+    arr[((10 * RESOLUTION) + 12) as usize] = true; // 212
+    arr[((11 * RESOLUTION) + 11) as usize] = true; // 221
+    arr[((12 * RESOLUTION) + 11) as usize] = true; // 241
+
+    arr[((15 * RESOLUTION) + 7) as usize] = true; // 211
+    arr[((15 * RESOLUTION) + 8) as usize] = true; // 212
+    arr[((15 * RESOLUTION) + 9) as usize] = true; // 221
+    arr[((16 * RESOLUTION) + 7) as usize] = true; // 241
+    arr[((16 * RESOLUTION) + 9) as usize] = true; // 241
+    arr[((16 * RESOLUTION) + 10) as usize] = true; // 241
+    arr[((16 * RESOLUTION) + 11) as usize] = true; // 241
+
+    arr
+}
+
+fn build_space_3
+()
+-> [bool; SQUARE as usize]
+{
+    let mut arr: [bool; SQUARE as usize] = [false; SQUARE as usize];
+
+    arr[((2 * RESOLUTION) + 3) as usize] = true; 
+    arr[((2 * RESOLUTION) + 4) as usize] = true; 
+    arr[((2 * RESOLUTION) + 5) as usize] = true; 
+    arr[((3 * RESOLUTION) + 3) as usize] = true; 
+    arr[((3 * RESOLUTION) + 5) as usize] = true; 
+    arr[((4 * RESOLUTION) + 3) as usize] = true; 
+    arr[((4 * RESOLUTION) + 5) as usize] = true; 
+    arr[((5 * RESOLUTION) + 3) as usize] = true; 
+    arr[((5 * RESOLUTION) + 4) as usize] = true; 
+    arr[((5 * RESOLUTION) + 5) as usize] = true; 
+
+
+
+    // arr[((15 * RESOLUTION) + 7) as usize] = true; // 211
+    // arr[((15 * RESOLUTION) + 8) as usize] = true; // 212
+    // arr[((15 * RESOLUTION) + 9) as usize] = true; // 221
+    // arr[((16 * RESOLUTION) + 7) as usize] = true; // 241
+    // arr[((16 * RESOLUTION) + 9) as usize] = true; // 241
+    // arr[((16 * RESOLUTION) + 10) as usize] = true; // 241
+    // arr[((16 * RESOLUTION) + 11) as usize] = true; // 241
+
     arr
 }
