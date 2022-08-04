4e0f3fd425e2248533f90c7dbb6e95d8c0389565
J Wylie Cullick <wylie@terebinth.is>
2022-08-04 10:34:51 -0500

    removed redundant logic

--- src/simple_2.rs
+++ src/simple_2.rs
@@ -57,16 +57,12 @@ fn count_neighborhoods(arr: &[bool; SQUARE])
                         east,
                     );
                     if eni > -1 { 
-                        if eni != (cursor_n_idx as i32) {
-                            // Not already in the right neighborhood
-                            let mut temp = (nc[cursor_n_idx]).clone();
-                            for el in &(nc[eni as usize]) {
-                                temp.insert(*el);
-                            }
-                            nc[cursor_n_idx] = temp;
-                            nc.remove(eni as usize);
+                        let mut temp = (nc[cursor_n_idx]).clone();
+                        for el in &(nc[eni as usize]) {
+                            temp.insert(*el);
                         }
-
+                        nc[cursor_n_idx] = temp;
+                        nc.remove(eni as usize);
                     } else {
                         nc[cursor_n_idx].insert(east);
                     }
@@ -80,14 +76,12 @@ fn count_neighborhoods(arr: &[bool; SQUARE])
                         south,
                     );
                     if eni > -1 {
-                        if eni != (cursor_n_idx as i32) {
-                            let mut temp = (nc[cursor_n_idx]).clone();
-                            for el in &(nc[eni as usize]) {
-                                temp.insert(*el);
-                            }
-                            nc[cursor_n_idx] = temp;
-                            nc.remove(eni as usize);
+                        let mut temp = (nc[cursor_n_idx]).clone();
+                        for el in &(nc[eni as usize]) {
+                            temp.insert(*el);
                         }
+                        nc[cursor_n_idx] = temp;
+                        nc.remove(eni as usize);
                     } else {
                         nc[cursor_n_idx].insert(south);
                     }
